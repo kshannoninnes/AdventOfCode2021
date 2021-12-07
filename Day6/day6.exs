8ec3e36734596f147b9df51be789ba62d373549e
K. Shannon-Innes <mav@hotmail.com.au>
2021-12-07 09:24:12 +0800

    Remove extra enumeration

--- Day6/day6.exs
+++ Day6/day6.exs
@@ -10,19 +10,17 @@ end
 defmodule Part1 do
   import Helpers
 
-  def update_fish(fish_map, day, days_max) when day == days_max, do: fish_map
-  def update_fish(fish_map, day, days_max) do
+  def update_fish(_, day, days_max, count) when day == days_max, do: count
+  def update_fish(fish_map, day, days_max, count) do
     new_borns = Map.get(fish_map, day - 7, 0) + Map.get(fish_map, day - 9, 0)
     new_map = Map.update(fish_map, day, new_borns, fn x -> x + new_borns end)
-    update_fish(new_map, day + 1, days_max)
+    update_fish(new_map, day + 1, days_max, count + Map.get(new_map, day))
   end
 
   def solve(file_path, days) do
     initial_fish = file_path |> get_input
 
-    length(initial_fish) + (Enum.frequencies(initial_fish)
-    |> update_fish(1, days)
-    |> Map.values
-    |> Enum.sum)
+    Enum.frequencies(initial_fish)
+    |> update_fish(1, days, length(initial_fish))
   end
 end
