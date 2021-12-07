61556efec664f78e40639e15c25172480f89e0cb
K. Shannon-Innes <mav@hotmail.com.au>
2021-12-07 20:15:22 +0800

    Update day 7 to more elegant solutions

--- Day7/day7.exs
+++ Day7/day7.exs
@@ -5,34 +5,43 @@ defmodule Helpers do
     |> String.split([",", "\n"], trim: true)
     |> Enum.map(&String.to_integer/1)
   end
-
-  def num_moves_to_target(target, list, step_func) do
-    Enum.reduce(list, 0, fn x, acc -> step_func.(x, target) + acc end)
-  end
-
-  def solve(file_path, step_func) do
-    input = file_path |> get_input
-    {min, max} = Enum.min_max(input)
-
-    min..max
-    |> Enum.map(fn x -> num_moves_to_target(x, input, step_func) end)
-    |> Enum.min
-  end
 end
 
 
-# Had a solution that used the median for part 1, but couldn't work out part 2
-# Used brute force for both in the end just to keep things tidy
 defmodule Part1 do
+  @moduledoc """
+    Using the median as the convergence point. If the input size is of even length,
+    either of the two median numbers will work
+  """
   import Helpers
 
   def step_one(x, target), do: abs(x - target)
-  def solve(file_path), do: Helpers.solve(file_path, &step_one/2)
+
+  def solve(file_path) do
+    input = file_path |> get_input |> Enum.sort
+    median = Enum.at(input, (length(input) / 2) |> ceil)
+
+    Enum.reduce(input, 0, fn x, acc -> step_one(x, median) + acc end)
+  end
 end
 
+
 defmodule Part2 do
+  @moduledoc """
+    Using upper and lower bound means and taking the minimum solution. No idea if
+    there's a closed-form solution for part 2. If there is, it's way more complicated
+    than it should be for day 7.
+  """
   import Helpers
 
-  def step_exp(x, target), do: 0..abs(x - target - 1) |> Enum.sum
-  def solve(file_path), do: Helpers.solve(file_path, &step_exp/2)
+  def step_exp(x, target), do: 0..abs(x - target) |> Enum.sum
+
+  def solve(file_path) do
+    input = file_path |> get_input
+    mean = Enum.sum(input) / length(input)
+
+    min(
+    Enum.reduce(input, 0, fn x, acc -> step_exp(x, mean |> ceil) + acc end),
+    Enum.reduce(input, 0, fn x, acc -> step_exp(x, mean |> floor) + acc end))
+  end
 end
